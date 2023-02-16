8b451364ff9bbc8d713388cebde36491defd5fd2
Zoe Spellman <zoe.spellman@canonical.com>
2023-02-16 15:17:14 -0800

    make script exit with error on error

--- tests/run.sh
+++ tests/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if ! snapctl is-connected "gaming-mesa"; then
-  echo "ERROR: not connected to the gaming-mesa content interface."
-  echo "To connect:"
-  echo "sudo snap connect gaming-graphics-tests:gaming-mesa gaming-graphics-core22"
-  exit 1
-fi
+# if ! snapctl is-connected "gaming-mesa"; then
+#   echo "ERROR: not connected to the gaming-mesa content interface."
+#   echo "To connect:"
+#   echo "sudo snap connect gaming-graphics-tests:gaming-mesa gaming-graphics-core22"
+#   exit 1
+# fi
 
 dir=$(dirname $0)
 `$dir/glx` && echo "GLX passed" || echo "GLX failed"
+glx_failed=$?
 `$dir/vulkan` && echo "Vulkan passed" || echo "Vulkan failed"
+vulkan_failed=$?
+
+exit $glx_failed || $vulkan_failed
