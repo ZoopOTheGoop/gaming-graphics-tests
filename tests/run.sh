a5625307836235b3a4821ce92032be7131877268
Zoe Spellman <zoe.spellman@canonical.com>
2023-02-16 14:14:01 -0800

    fail when plugs aren't connected

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! snapctl is-connected "gaming-mesa"; then
+  echo "ERROR: not connected to the gaming-mesa content interface."
+  echo "To connect:"
+  echo "sudo snap connect gaming-graphics-tests:gaming-mesa gaming-graphics-core22"
+  exit 1
+fi
+
 dir=$(dirname $0)
 `$dir/glx` && echo "GLX passed" || echo "GLX failed"
 `$dir/vulkan` && echo "Vulkan passed" || echo "Vulkan failed"
