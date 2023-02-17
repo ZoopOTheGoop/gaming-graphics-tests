07330b41597e17614df604d15e1422affb7a951d
Zoe Spellman <zoe.spellman@canonical.com>
2023-02-16 16:44:31 -0800

    Add back plug check

--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# if ! snapctl is-connected "gaming-mesa"; then
-#   echo "ERROR: not connected to the gaming-mesa content interface."
-#   echo "To connect:"
-#   echo "sudo snap connect gaming-graphics-tests:gaming-mesa gaming-graphics-core22"
-#   exit 1
-# fi
+if ! snapctl is-connected "gaming-mesa"; then
+  echo "ERROR: not connected to the gaming-mesa content interface."
+  echo "To connect:"
+  echo "sudo snap connect gaming-graphics-tests:gaming-mesa gaming-graphics-core22"
+  exit 1
+fi
 
 dir=$(dirname $0)
 
